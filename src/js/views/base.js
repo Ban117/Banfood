31a5159b921fd54b7467f1e2640a93534ae96426
Ban <bcam117@gmail.com>
2018-09-30 20:49:00 +0200

    Reduced length of results Display loader

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,7 +1,30 @@
 // All elements we need from our DOM
+// And things which are used everywhere
 
 export const elements = {
   searchInput: document.querySelector('.search__field'),
   searchForm: document.querySelector('.search'),
-  searchResList: document.querySelector('.results__list')
+  searchRes: document.querySelector('.results'),
+  searchResList: document.querySelector('.results__list'),
+};
+
+export const elementStrings = {
+  loader: 'loader'
+};
+
+// Not in search view because used in multiple places
+export const renderLoader = parent => {
+  const loader = `
+    <div class="${elementStrings.loader}">
+      <svg>
+        <use href="img/icons.svg#icon-cw"></use>
+      </svg>
+    </div>
+  `;
+  parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+export const clearLoader = () => {
+  const loader = document.querySelector(`.${elementStrings.loader}`);
+  if (loader) loader.parentElement.removeChild(loader);
 }

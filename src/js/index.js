d6c66d25412273e43cef6fa137bc2fc5f7c16446
Ban <bcam117@gmail.com>
2018-09-30 18:10:26 +0200

    Created basic search model, added event listener to search and rendering results to UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,47 @@
-import string from './models/Search';
-import {add, multiply, ID} from './views/searchView';
+import Search from './models/Search'
+import * as searchView from './views/searchView'
+import {elements} from './views/base'
 
-console.log(`Using imported functions add ${add(2,3,)}`);
+// Controller for the whole app
 
-console.log(string);
+
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+
+const state = {};
+
+const controlSearch = async () => {
+  // 1) Get query from view
+  const query = searchView.getInput();
+  console.log(query);
+  
+  if (query) {
+    // 2) New search object and add to state
+    state.search = new Search(query);
+
+    // 3) Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+
+
+    // 4) Search for recipes
+    await state.search.getResults();
+
+    // 5) Render results on UI
+    // console.log(state.search.result);
+    searchView.renderResults(state.search.result);
+    
+
+  }
+
+}
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,22 @@
-export default 'I am an exported string';
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    const key = '22de6ab78bfc5ee2756c45aaccbd8ad7';
+    // Could use fetch but yeah
+    // This'll work on all browsers
+    // at least it automatically returns to JSON
+    try {
+      const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+      this.result = res.data.recipes;
+      // console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+  
+}

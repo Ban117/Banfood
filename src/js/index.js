82d0b0abd69638e5edff4f1c879412f24b04d39f
Ban <bcam117@gmail.com>
2018-09-30 15:36:17 +0200

    Fixed bable nonesense

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
-// Global app controller
+import string from './models/Search';
+import {add, multiply, ID} from './views/searchView';
+
+console.log(`Using imported functions add ${add(2,3,)}`);
+
+console.log(string);
